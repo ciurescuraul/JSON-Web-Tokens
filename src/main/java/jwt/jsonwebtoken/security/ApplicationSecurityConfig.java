82a9dff4f009ac2b72744e78a08fd8ab40e10fd6
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-31 21:11:36 +0300

    JWT: Filter (in progress)

--- src/main/java/jwt/jsonwebtoken/security/ApplicationSecurityConfig.java
+++ src/main/java/jwt/jsonwebtoken/security/ApplicationSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import jwt.jsonwebtoken.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
@@ -12,10 +14,11 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
                 .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authenticated();
     }
 }
