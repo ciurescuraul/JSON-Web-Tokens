b56ba7c24e1d19813b97659d852d069d90673766
Raul Ciurescu <raul.ciurescu@computaris.com>
2021-05-31 17:35:51 +0300

    Create a simple API endpoint

---
+++ src/main/java/jwt/jsonwebtoken/student/Student.java
@@ -0,0 +1,24 @@
+package jwt.jsonwebtoken.student;
+
+public class Student
+{
+
+  private final Integer id;
+  private final String  studentName;
+
+  public Student(Integer id, String studentName)
+  {
+    this.id = id;
+    this.studentName = studentName;
+  }
+
+  public Integer getId()
+  {
+    return id;
+  }
+
+  public String getStudentName()
+  {
+    return studentName;
+  }
+}
